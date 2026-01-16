2e5a741585047b2a44edcc35a0bd84e0b0c58793
SACHIN SINGH <sachin8174056@gmail.com>
2026-01-16 13:41:06 +0530

    update

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Register Page</h1>
+    </div>
+  );
+};
+
+export default Register;
