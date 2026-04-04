7c5fbeb69b469ae812122ed174952e550fb9392f
SACHIN SINGH <sachin8174056@gmail.com>
2026-04-04 09:53:37 +0530

    Clear

--- frontend/src/api/authApi.js
+++
@@ -1,9 +0,0 @@
-import axiosInstance from "../utils/axiosInstance";
-
-export const registerUser = (data) => {
-  return axiosInstance.post("/register", data);
-};
-
-export const loginUser = (data) => {
-  return axiosInstance.post("/login", data);
-};
